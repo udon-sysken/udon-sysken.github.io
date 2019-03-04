32aa2aa7e781e2d4eb1fe1f796ebd21898e7feb1
umbroom <32835505+umbroom@users.noreply.github.com>
2019-03-04 22:50:57 +0900

    no one in here

--- main.js
+++ main.js
@@ -1,220 +1,202 @@
 ﻿function rand(n){
-    return Math.floor(Math.random() * n);
+    return Math.floor(Math.random() * n + 1);
 }
 
-function draw1(){
-    var canvas = document.getElementById('background1');
-    if(!canvas || !canvas.getContext)
-        return -1;
-
-    canvas.width = screen.width +40;
-    canvas.height = screen.height +20;
-
-    var ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = "rgba(0, 175, 255, 0.4)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    var r = 60;//正三角形の1辺（全体的に解像度を無視している感）
-    ctx.fillStyle = "rgba(0, 175, 255, 0.1)";
-
-    //ランダムに正六角形を配置する（各正三角形にランダムに色を配置した方が良いが向き考えるの面倒　誰かやって）
-    for(var i=0; i<canvas.width*canvas.height/1000; i++){
-        let x = rand(canvas.width/r/0.866 + 1);
-        let y = r*rand(canvas.height/r + 1) + x%2*r/2;
-        x *= r*0.866;
-        
-        ctx.beginPath();
-        ctx.moveTo(x, y-r);
-        ctx.lineTo(x+r*0.866, y-r/2);
-        ctx.lineTo(x+r*0.866, y+r/2);
-        ctx.lineTo(x, y+r);
-        ctx.lineTo(x-r*0.866, y+r/2);
-        ctx.lineTo(x-r*0.866, y-r/2);
-        ctx.closePath();
-        ctx.fill();
-    }
-}
-
-function draw2(){
-    var canvas = document.getElementById('background2');
-    if(!canvas || !canvas.getContext)
-        return -1;
-
-    canvas.width = screen.width +40;
-    canvas.height = screen.height +20;
-
-    var ctx = canvas.getContext('2d');
-
-    var r = 60;//正三角形の1辺（全体的に解像度を無視して
-
-    //正六角形の輪郭（各辺に(ry
-    ctx.strokeStyle = "rgba(255, 255, 255, 0.2)";
-    for(var i=0; i<canvas.width*canvas.height/5000; i++){
-        let x = rand(canvas.width/r/1.732 + 1);
-        let y = 2*r*rand(canvas.height/r/2 + 1) + x%2*r;
-        x *= r*1.732;
-        
-        ctx.beginPath();
-        ctx.moveTo(x, y-2*r);
-        ctx.lineTo(x+r*1.732, y-r);
-        ctx.lineTo(x+r*1.732, y+r);
-        ctx.lineTo(x, y+2*r);
-        ctx.lineTo(x-r*1.732, y+r);
-        ctx.lineTo(x-r*1.732, y-r);
-        ctx.closePath();
-        ctx.stroke();
-    }
-
-    //ひし形ではない何か
-    ctx.strokeStyle = "rgba(255, 255, 255, 0.05)";
-    for(var i=0; i<canvas.width*canvas.height/25000; i++){
-        let x = rand(canvas.width/r/1.732 + 1);
-        let y = 2*r*rand(canvas.height/r/2 + 1) + x%2*r;
-        x *= r*1.732;
-        
-        ctx.beginPath();
-        ctx.moveTo(x, y-2*r);
-        ctx.lineTo(x-r*1.732, y+r);
-        ctx.closePath();
-        ctx.stroke();
-        
-        ctx.beginPath();
-        ctx.moveTo(x+r*1.732, y-r);
-        ctx.lineTo(x, y+2*r);
-        ctx.closePath();
-        ctx.stroke();
-        
-        ctx.beginPath();
-        ctx.moveTo(x, y-2*r);
-        ctx.lineTo(x+r*1.732, y+r);
-        ctx.closePath();
-        ctx.stroke();
-        
-        ctx.beginPath();
-        ctx.moveTo(x, y+2*r);
-        ctx.lineTo(x-r*1.732, y-r);
-        ctx.closePath();
-        ctx.stroke();
-    }
-}
-
-var triangle_data;
-function draw3(){
-    canvas3 = document.getElementById('background3');
-    if(!canvas3 || !canvas3.getContext)
-        return -1;
-
-    canvas3.width = screen.width +40;
-    canvas3.height = screen.height +20;
-
-    ctx3 = canvas3.getContext('2d');
-    var r = 60;//正三角形の1辺（全体的に解像度を無視している感）
-
-    l = 40;
-    ctx3.fillStyle = "rgba(12, 19, 117, 0.2)";
-
-    triangle_data = [];
-
-    //破片
-    for(var i=0; i<Math.floor(canvas3.width*canvas3.height/20000); i++){
-        let x = rand(canvas3.width + 1);
-        let y = rand(canvas3.height+1);
-        
-        triangle_data[i] = [];
-        triangle_data[i][0] = (rand(l*2)-l);
-        triangle_data[i][1] = (rand(l*2)-l);
-        triangle_data[i][2] = (rand(l*2)-l);
-        triangle_data[i][3] = (rand(l*2)-l);
-        triangle_data[i][4] = (rand(l*2)-l);
-        triangle_data[i][5] = (rand(l*2)-l);
-        triangle_data[i][6] = rand(360);
-        triangle_data[i][7] = rand(360);
-        triangle_data[i][8] = rand(360);
-        triangle_data[i][9] = 2*(rand(5)-2);
-        triangle_data[i][10] = 2*(rand(5)-2);
-        triangle_data[i][11] = 2*(rand(5)-2);
-        triangle_data[i][12] = x;
-        triangle_data[i][13] = y;
-    }
-    window.requestAnimationFrame(fragment_anime);
-}
-
-radian = function(degree){
+function radian(degree){
     return degree / 180 * Math.PI
 }
 
-rotate = function(x0, y0, tx, ty, tz){
-    let z1 = y0*Math.sin(radian(tx));
-    let y1 = y0*Math.cos(radian(tx));
-
-    let x1 = z1*Math.sin(radian(ty)) + x0*Math.cos(radian(ty));
-
-    let x2 = x1*Math.cos(radian(tz)) - y1*Math.sin(radian(tz));
-    let y2 = x1*Math.sin(radian(tz)) + y1*Math.cos(radian(tz));
-
-    var xy = [x2, y2];
-    return xy;
+function rate_area(n){
+    return Math.floor(canvas_width*canvas_height / n);
 }
 
-var start_time = 1;
-fragment_anime = function(timestamp){
-    ctx3.clearRect(0, 0, canvas3.width, canvas3.height);
-    for(var i=0; i<triangle_data.length; i++){
-        triangle_data[i][12]+=5;
-        triangle_data[i][6] += triangle_data[i][9];
-        triangle_data[i][7] += triangle_data[i][10];
-        triangle_data[i][8] += triangle_data[i][11];
+function open_canvas(id){
+    var canvas = document.getElementById(id);
+    if(!canvas || !canvas.getContext)
+        return -1;
+
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+
+    return canvas;
+}
+
+function draw_hexagon_tile(){
+    ctx1.fillStyle = "rgba(0, 175, 255, 0.4)";
+    ctx1.fillRect(0, 0, canvas_width, canvas_height);
+
+    ctx1.fillStyle = "rgba(0, 175, 255, 0.1)";
+    for(var i=0; i<rate_area(1000); i++){
+        let x = rand(canvas_width/r/0.866);
+        let y = r*rand(canvas_height/r) + x%2*r/2;
+        x *= r*0.866;
+        
+        ctx1.beginPath();
+        ctx1.moveTo(x, y-r);
+        ctx1.lineTo(x+r*0.866, y-r/2);
+        ctx1.lineTo(x+r*0.866, y+r/2);
+        ctx1.lineTo(x, y+r);
+        ctx1.lineTo(x-r*0.866, y+r/2);
+        ctx1.lineTo(x-r*0.866, y-r/2);
+        ctx1.closePath();
+        ctx1.fill();
+    }
+}
+
+function draw_diamond_line(){
+    ctx2.strokeStyle = "rgba(0, 175, 255, 0.2)";
+    for(var i=0; i<rate_area(5000); i++){
+        let x = rand(canvas_width/r/1.732);
+        let y = 2*r*rand(canvas_height/r/2) + x%2*r;
+        x *= r*1.732;
+        
+        ctx2.beginPath();
+        ctx2.moveTo(x, y-2*r);
+        ctx2.lineTo(x+r*1.732, y-r);
+        ctx2.lineTo(x+r*1.732, y+r);
+        ctx2.lineTo(x, y+2*r);
+        ctx2.lineTo(x-r*1.732, y+r);
+        ctx2.lineTo(x-r*1.732, y-r);
+        ctx2.closePath();
+        ctx2.stroke();
+    }
+
+    ctx2.strokeStyle = "rgba(0, 175, 225, 0.05)";
+    for(var i=0; i<rate_area(25000); i++){
+        let x = rand(canvas_width/r/1.732);
+        let y = 2*r*rand(canvas_height/r/2) + x%2*r;
+        x *= r*1.732;
+        
+        ctx2.beginPath();
+        ctx2.moveTo(x, y-2*r);
+        ctx2.lineTo(x-r*1.732, y+r);
+        ctx2.closePath();
+        ctx2.stroke();
+        
+        ctx2.beginPath();
+        ctx2.moveTo(x+r*1.732, y-r);
+        ctx2.lineTo(x, y+2*r);
+        ctx2.closePath();
+        ctx2.stroke();
+        
+        ctx2.beginPath();
+        ctx2.moveTo(x, y-2*r);
+        ctx2.lineTo(x+r*1.732, y+r);
+        ctx2.closePath();
+        ctx2.stroke();
+        
+        ctx2.beginPath();
+        ctx2.moveTo(x, y+2*r);
+        ctx2.lineTo(x-r*1.732, y-r);
+        ctx2.closePath();
+        ctx2.stroke();
+    }
+}
+
+function set_fragment(i, x){
+    fragments[i]  = {
+        points : [
+            {x : rand(l*2)-l, y : rand(l*2)-l},
+            {x : rand(l*2)-l, y : rand(l*2)-l},
+            {x : rand(l*2)-l, y : rand(l*2)-l}
+        ],
+        center : {
+            x : x,
+            y : rand(canvas_height)
+        },
+        angle : {
+            x : rand(360),
+            y : rand(360),
+            z : rand(360)
+        },
+        angular_speed : {
+            x : 2*(rand(5)-2),
+            y : 2*(rand(5)-2),
+            z : 2*(rand(5)-2)
+        }
+    };
+}
+
+function rotate(f, i){
+    var x0 = f.points[i].x;
+    var y0 = f.points[i].y;
+    var tx = radian(f.angle.x);
+    var ty = radian(f.angle.y);
+    var tz = radian(f.angle.z);
+
+    var z1 = y0*Math.sin(tx);
+    var y1 = y0*Math.cos(tx);
+    var x1 = z1*Math.sin(ty) + x0*Math.cos(ty);
+
+    var x2 = x1*Math.cos(tz) - y1*Math.sin(tz);
+    var y2 = x1*Math.sin(tz) + y1*Math.cos(tz);
+
+    return {x : x2, y : y2};
+}
+
+function draw_fragment(timestamp){
+    ctx3.clearRect(0, 0, canvas_width, canvas_height);
+
+    for(var i=0; i<fragments.length; i++){
+        f = fragments[i];
+        f.center.x += 5;
+        f.angle.x += f.angular_speed.x;
+        f.angle.y += f.angular_speed.y;
+        f.angle.z += f.angular_speed.z;
 
         ctx3.beginPath();
-        let xy = rotate(triangle_data[i][0], triangle_data[i][1], triangle_data[i][6], triangle_data[i][7], triangle_data[i][8]);
-        ctx3.moveTo(triangle_data[i][12]+xy[0], triangle_data[i][13]+xy[1]);
-        xy = rotate(triangle_data[i][2], triangle_data[i][3], triangle_data[i][6], triangle_data[i][7], triangle_data[i][8]);
-        ctx3.lineTo(triangle_data[i][12]+xy[0], triangle_data[i][13]+xy[1]);
-        xy = rotate(triangle_data[i][4], triangle_data[i][5], triangle_data[i][6], triangle_data[i][7], triangle_data[i][8]);
-        ctx3.lineTo(triangle_data[i][12]+xy[0], triangle_data[i][13]+xy[1]);
+        let xy;
+        xy = rotate(f, 0);
+        ctx3.moveTo(f.center.x + xy.x, f.center.y + xy.y);
+        xy = rotate(f, 1);
+        ctx3.lineTo(f.center.x + xy.x, f.center.y + xy.y);
+        xy = rotate(f, 2);
+        ctx3.lineTo(f.center.x + xy.x, f.center.y + xy.y);
         ctx3.closePath();
         ctx3.fill();
         
-        if(triangle_data[i][12]>canvas3.width){
-            let x = 0;
-            let y = rand(canvas3.height+1);
-        triangle_data[i][0] = rand(l*2)-l;
-        triangle_data[i][1] = rand(l*2)-l;
-        triangle_data[i][2] = rand(l*2)-l;
-        triangle_data[i][3] = rand(l*2)-l;
-        triangle_data[i][4] = rand(l*2)-l;
-        triangle_data[i][5] = rand(l*2)-l;
-        triangle_data[i][6] = rand(360);
-        triangle_data[i][7] = rand(360);
-        triangle_data[i][8] = rand(360);
-        triangle_data[i][9] = rand(2)==0 ? 2 : -2;
-        triangle_data[i][10] = rand(2)==0 ? 2 : -2;
-        triangle_data[i][11] = rand(2)==0 ? 2 : -2;
-        triangle_data[i][12] = x;
-        triangle_data[i][13] = y;
-        }
-
+        if(f.center.x>canvas_width)
+            set_fragment(i, 0);
     }
 
     if(timestamp/2000>=start_time){
         start_time++;
-        draw2();
+        ctx2.clearRect(0, 0, canvas_width, canvas_height);
+        draw_diamond_line();
     }
 
-    window.requestAnimationFrame(fragment_anime);
+    window.requestAnimationFrame(draw_fragment);
 }
 
 window.onload = function(){
-    draw1();
-    draw2();
-    draw3();
+    r = 60;
+    l = 40;
+    canvas_width = screen.width +40;
+    canvas_height = screen.height +20;
+    start_time = 1;
+    
+    fragments = [];
+    for(var i=0; i<rate_area(20000); i++)
+        set_fragment(i, rand(canvas_width));
+    
+    canvas1 = open_canvas('hexagon_tile');
+    ctx1 = canvas1.getContext('2d');
+
+    canvas2 = open_canvas('diamond_line');
+    ctx2 = canvas2.getContext('2d');
+
+    canvas3 = open_canvas('fragment');
+    ctx3 = canvas3.getContext('2d');
+
+    draw_hexagon_tile();
+    draw_diamond_line();
+
+    ctx3.fillStyle = "rgba(12, 19, 117, 0.2)";
+    window.requestAnimationFrame(draw_fragment);
 }
 
-//cssアニメーション用class発行
 window.onscroll = function(){
-    var y = Math.round(window.pageYOffset/window.innerHeight);
-    if(1<=y&&y<=2){
-        document.getElementsByClassName("content")[y-1].classList.add("active");
-    }
+    var y = Math.round(window.pageYOffset/window.innerHeight) -1;
+    if(0<=y&&y<=1)
+        document.getElementsByTagName("article")[y].classList.add("active");
 }
